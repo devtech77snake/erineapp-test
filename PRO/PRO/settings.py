b90793b77a518723524965fe913849e225f67cf2
devtech77snake <devtech77snake@outlook.com>
2024-03-14 09:50:40 -0400

    #7 fix cors error

--- PRO/PRO/settings.py
+++ PRO/PRO/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -139,19 +139,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # CORS
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000", 'https://localhost']
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000', 'http://127.0.0.1:8000', 'https://localhost'
+    'http://localhost:8000', 'http://127.0.0.1:8000', 'http://localhost'
 ]
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'X-CSRFToken',  # Make sure this is included
-    'x-requested-with',
-    # Add any other headers you are using
-]
+
+CORS_ALLOW_ALL_HEADERS = True
